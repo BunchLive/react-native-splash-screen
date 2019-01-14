7e628be3a85af31a4335938dfc3ca996ec2460b3
Jordan Howlett <jordan@howlett.io>
2019-01-14 17:00:00 -0500

    Try this

--- ios/RNSplashScreen.m
+++ ios/RNSplashScreen.m
@@ -34,7 +34,8 @@ RCT_EXPORT_MODULE(SplashScreen)
 
 + (void)showSplash:(NSString*)splashScreen inRootView:(UIView*)rootView {
     if (!loadingView) {
-        loadingView = [[[NSBundle mainBundle] loadNibNamed:splashScreen owner:self options:nil] objectAtIndex:0];
+        // loadingView = [[[NSBundle mainBundle] loadNibNamed:splashScreen owner:self options:nil] objectAtIndex:0];
+        loadingView = [[SplashViewController alloc] initWithNibName:@"SplashViewController" bundle:nil];
         CGRect frame = rootView.frame;
         frame.origin = CGPointMake(0, 0);
         loadingView.frame = frame;
